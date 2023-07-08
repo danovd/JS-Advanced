1f88402e5ff67da77420aa4f94b03536f1bf9e0b
danovd <danovski_dimitar@abv.bg>
2023-07-08 08:23:30 +0300

    String Length

---
+++ _03_Syntax_Lab/_02_String_Length.js
@@ -0,0 +1,6 @@
+function String(a, b, c){
+    let total = a.length + b.length + c.length;
+    let averageLength = Math.floor(total / 3);
+    console.log(total);
+    console.log(averageLength);     
+}
