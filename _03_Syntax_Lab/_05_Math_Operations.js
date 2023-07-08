a915ce3670f0a06ee7a5f1424e2964c4cc6d169e
danovd <danovski_dimitar@abv.bg>
2023-07-08 08:25:28 +0300

    Math Operations

---
+++ _03_Syntax_Lab/_05_Math_Operations.js
@@ -0,0 +1,13 @@
+function action(num1, num2, operator){
+let result;
+switch (operator){
+    case '+': result = num1 + num2; break;
+    case '-': result = num1 - num2; break;
+    case '/': result = num1 / num2; break;
+    case '*': result = num1 * num2; break;
+    case '%': result = num1 % num2; break;
+    case '**': result = num1 ** num2; break;
+}
+console.log(result);
+
+}
